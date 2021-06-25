e6525001e014819eaee454dedef4141f637d404d
Sakib Noman <skbnmn@gmail.com>
2021-06-26 00:09:49 +0600

    reducer

---
+++ src/store/reducers/Data_reducer.js
@@ -0,0 +1,20 @@
+import { ADD_NEW_DATA, SELECT_DATA } from "../actions";
+
+const initialState = ['hello world', 'learn Redux'];
+
+function Data(state = initialState, action) {
+    switch (action.type) {
+        case ADD_NEW_DATA:
+            let newState = state;
+            newState.push(action.payload);
+            state = newState;
+            return state;
+        //end
+        case SELECT_DATA:
+            return state;
+        default:
+            return state;
+    }
+}
+
+export default Data;
