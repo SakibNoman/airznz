b10f492d48e538ee04c91a3b49cb4f02d70ff847
Sakib Noman <skbnmn@gmail.com>
2021-06-26 00:03:45 +0600

    action

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React from 'react';
+import { createStore } from 'redux';
 import './App.css';
 import Counter from './Components/counter';
 import Datalist from './Components/datalist';
 import Main from './Components/main';
+
+//actions
+
+const increment = () => {
+  return { type: 'INCREMENT' }
+}
+
+const decrement = () => {
+  return { type: 'DECREMENT' }
+}
+
+//reducer
+
+const reducerCounter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+//store
+
+let store = createStore(reducerCounter);
+
+//display
+
+store.subscribe(() => (console.log(store.getState())))
+
+//dispatch
+
+store.dispatch(increment());
+store.dispatch(decrement());
+
 
 
 
--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -7,15 +7,15 @@ class main extends Component {
                 <div className="card text-white bg-success mb-3" >
                     <div className="card-header text-center">Main</div>
                     <div className="card-body text-center">
-                        <h5 className="card-title">In&DE Crement Number</h5>
+                        <h5 className="card-title">In&Decrement Number</h5>
                         <button
-                            className='btn btn-sucsses m-2'
+                            className='btn btn-dark m-2'
                         >Increment
                         </button>
                         <button className='btn btn-danger m-2'  >Decrement
                         </button>
                         <div className='text-center'>
-                            <input placeholder='add new data' className='btn btn-group m-2' />
+                            <input placeholder='add new data' className='btn bg-white btn-group m-2' />
                             <button className='btn btn-primary m-2' >Add</button>
                         </div>
                     </div>
